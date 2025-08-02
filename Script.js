98d6a2dd90cc4709146dcdbbbae5680e3532ac33
ahsan0-del <a12400786@gmail.com>
2025-08-03 01:38:18 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,11 +2,11 @@ document.getElementById("connectButton").onclick = async function() {
   if (window.ethereum) {
     try {
       const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
-      alert("Connected: " + accounts[0]);
+      alert("Your wallet is connected. Tokens will be sent to:\n" + accounts[0] + "\n\nPlease wait for distribution.");
     } catch (error) {
       alert("Error connecting: " + error.message);
     }
   } else {
-    alert("Please install MetaMask!");
+    alert("Please install MetaMask to check your wallet!");
   }
 };
